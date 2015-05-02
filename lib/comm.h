737a706837fc1ae06abdd9079edfe2c2d442a2ca
YHM07 <yangheming55@126.com>
2015-05-02 23:11:03 +0800

    change comm.h to adaptive the template

--- lib/comm.h
+++ lib/comm.h
@@ -10,38 +10,12 @@
 
 #include <stdio.h>
 #include <iostream>
-using namespace std;
+//using namespace std;
 
-/**
- * @brief	print 
- *
- * @tparam T
- * @param	A[]
- * @param	n
- */
 template <class T>
-void print(const T A[],int n)
-{
-	for (int i=0;i < n; ++i){
-		cout << A[i] << "\t";
-	}
-	cout << endl;
-}
-
-/**
- * @brief	swap 
- *
- * @tparam T
- * @param	p
- * @param	q
- */
+void print(const T A[],int n);
 template <class T>
-void swap(T *p,T *q)
-{
-	T tmp;
-	tmp = *p;
-	*p  = *q;
-	*q  = tmp;
-}
+void swap(T *p,T *q);
+#include "comm-inl.h"
 #endif  /* ---- not COMM_H_*/
 
